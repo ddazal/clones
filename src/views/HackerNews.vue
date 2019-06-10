64bf4c3023ad82e52d62281de522a0b1aec05970
David Daza <03dazal@gmail.com>
2019-06-10 06:26:13 -0500

    add vue-meta for proper tabs title

--- src/views/HackerNews.vue
+++ src/views/HackerNews.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="hackernews" class="container mx-auto sm:my-2 sm:px-12">
+  <div id="hackernews" class="mx-auto sm:my-2 sm:w-5/6 w-full">
     <HackerNewsNav />
   </div>
 </template>
@@ -9,6 +9,9 @@ import HackerNewsNav from '@/components/HackerNewsNav'
 
 export default {
   name: 'HackerNews',
-  components: { HackerNewsNav }
+  components: { HackerNewsNav },
+  metaInfo: {
+    title: 'Hackernews | Clones'
+  }
 }
 </script>
